0ff7c5513b62109a0e16868065fe6fa7fcfa16de
Carrima Hewitt <chewitt1@ufl.edu>
2023-03-19 20:10:16 -0500

    Add 02_repeatString Solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num > 0){
+        if(str != ""){
+            let outVal = str;
 
+            for(let i = 0; i < (num-1); i++){
+                outVal += str;
+            }
+            return outVal;
+        }
+    }
+    else if(num < 0){
+        return "ERROR";
+    }
+    return "";
 };
 
 // Do not edit below this line
