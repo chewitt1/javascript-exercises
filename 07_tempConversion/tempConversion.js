c8f08b39fabaf235194838fbca87a9e4fd72568b
Carrima Hewitt <chewitt1@ufl.edu>
2023-04-16 17:16:37 -0400

    Complete 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = ((f-32)*(5/9)).toFixed(1);
+  if(c % 1 == 0){
+    return Math.floor(c);
+  }
+  return parseFloat(c);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = ((c*(9/5))+32).toFixed(1);
+  if(f % 1 == 0){
+    return Math.floor(f);
+  }
+  return parseFloat(f);
 };
 
 // Do not edit below this line
