a9646ccc368d4de7461bd3d0592131c1fe6ec395
Carrima Hewitt <chewitt1@ufl.edu>
2023-04-16 17:00:05 -0400

    Complete 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(typeof(a)== "number" && typeof(b)== "number"){
+        if(a > 0 && b > 0){
+            let sum = 0;
+            if(a < b){
+                for(i = a; i < (b+1); i++){
+                    sum += i;
+                }
+                return sum;
+            }
+            else{
+                for(i = b; i < (a+1); i++){
+                    sum += i;
+                }
+                return sum;
+            }
+        }
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
