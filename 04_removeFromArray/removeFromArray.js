dc8bdc1eaa4ed933d4c865a481a7d8cec0762cd6
Carrima Hewitt <chewitt1@ufl.edu>
2023-04-16 16:51:49 -0400

    Complete 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
+    arr = arguments[0];
+    for (var i=1; i<arguments.length; i++){
+        let j = arr.indexOf(arguments[i]);
+        if(j != -1){
+            arr.splice(j,1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
