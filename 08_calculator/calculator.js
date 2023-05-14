54b5bf0e8d4e8e88c48d32fcc602ea3586dd4827
Carrima Hewitt <chewitt1@ufl.edu>
2023-05-13 22:21:09 -0400

    Solve 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if(arr.length > 0){
+    return arr.reduce((total, val) => {return total + val;}, 0);
+  }
+  return 0;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length > 0){
+    let val = 1;
+    for(let i = 0; i < arr.length; i++){
+      val *= arr[i];
+    }
+    return val;
+  }
+  return 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num == 0 || num == 1){
+    return 1;
+  }
+  return (num * factorial(num -1));
 };
 
 // Do not edit below this line
