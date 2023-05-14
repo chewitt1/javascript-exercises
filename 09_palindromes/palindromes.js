29aee0717a4fcab4c0692536cdeb38994b727b50
Carrima Hewitt <chewitt1@ufl.edu>
2023-05-13 23:02:28 -0400

    Solve 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
-
+const reverseString = function(strIn) {
+    strArr = strIn.split("");
+    strArr.reverse();
+    return strArr.join("");
 };
+
+const removePunctuation = function(strIn){
+    let ret = strIn;
+    ret = ret.replace(/!/g, "");
+    ret = ret.replace(/,/g, "");
+    ret = ret.replace(/\./g, "");
+    ret = ret.replace(/ /g, "");
+    return ret;
+};
+
+const palindromes = function (str) {
+    str = removePunctuation(str.toLowerCase());
+    let left = str.substring(0, ((str.length/2)));
+    let right = (str.length % 2 == 0) ? str.substring((str.length/2)) : str.substring((str.length/2) + 1);
+    let rLeft = reverseString(left);
+    return (rLeft == right);
+};
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
