cb03427d947121ae1b7bf6344607a01f766fae7b
Carrima Hewitt <chewitt1@ufl.edu>
2023-05-13 23:38:33 -0400

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    oldest = arr[0];
+    max = (arr[0].yearOfDeath != undefined) ? (arr[0].yearOfDeath -arr[0].yearOfBirth) : ((new Date().getFullYear()) - arr[0].yearOfBirth);
+    if(arr.length > 1){
+        for(let i = 1; i < arr.length; i++){
+            let curr = (arr[i].yearOfDeath != undefined) ? (arr[i].yearOfDeath -arr[i].yearOfBirth) : ((new Date().getFullYear()) - arr[i].yearOfBirth)
+            if(curr > max){
+                max = curr;
+                oldest = arr[i];
+            }
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
